2393e3e45f51ad4bacf5383760ae714c93f94dc9
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-03-07 17:19:18 -0700

    Complete Assignment: Time targeting

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,12 +1,13 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-# Assignment: Clean phone numbers 
+# Assignment: Clean phone numbers
 def clean_phone_numbers(phone_num)
   cleaned_num = phone_num.gsub(/\D/, '')
 
@@ -40,6 +41,15 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+# Assignment: Time targeting
+def fetch_reg_hour(time)
+  Time.strptime(time, '%D %R').hour
+end
+
+def peak_hour(hour_arr)
+  hour_arr.tally.select { |hour, count| count == hour_arr.tally.values.max }.keys.map { |hr| "#{hr}:00" }
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -51,18 +61,27 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+# Assignment: Time targeting
+reg_hour = []
+
 contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  phone_num = clean_phone_numbers(row[:homephone])
+  # id = row[0]
+  # name = row[:first_name]
+  # zipcode = clean_zipcode(row[:zipcode])
+  # phone_num = clean_phone_numbers(row[:homephone])
 
-  legislators = legislators_by_zipcode(zipcode)
+  # legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+  # form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id, form_letter)
+  # save_thank_you_letter(id, form_letter)
 
-  # Assignment: Clean phone numbers
-  puts "#{name} can sign up for mobile alerts for his number #{phone_num}" if phone_num != ''
+  # # Assignment: Clean phone numbers
+  # puts "#{name} can sign up for mobile alerts for his number #{phone_num}" if phone_num != ''
+
+  # Assignment: Time targeting
+  reg_hour.push(fetch_reg_hour(row[:regdate]))
 end
+
+# Assignment: Time targeting
+puts "The peak registration hours are #{peak_hour(reg_hour).join(', ')}."
